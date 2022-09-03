783eeaa209a5fa22e993985dd374f578917fed15
unexploredtest <unexploredtest@protonmail.com>
2022-09-03 13:48:05 +0430

    Added apps

---
+++ ModernWGC/ModernWGC_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ModernwgcApiConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "ModernWGC_api"
---
+++ ModernWGC/ModernWGC_ui/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ModernwgcUiConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "ModernWGC_ui"
